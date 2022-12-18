31c8aba832a5bbd31576bfa338318edc385c56a8
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-18 18:56:49 +0530

    Initial Commit

---
+++ Money-Split/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money-Split
+//
+//  Created by Sahid Reza on 18/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
